aa7b2c37b7d5310bea492491b1559cd9a81a8b9a
Jaime Rabasco Ronda <jaimerabasco@gmail.com>
2015-11-05 13:15:46 +0100

    Publicado_Api_Vibracion_v0.1

--- js/vibrar.js
+++ js/vibrar.js
@@ -1,7 +1,6 @@
 var repetir;
 function repetir_vibracion(){
 	repetir = setInterval(function(){navigator.vibrate(1000);},1500);
-	//repetir = setInterval(function(){alert("hola");},1500);
 }
 
 function parar_vibracion(){
@@ -14,10 +13,8 @@ if(window.navigator && window.navigator.vibrate){
 	var repetir;
 	document.querySelector("#vibrar").addEventListener("click", function(){navigator.vibrate(1000);},false);
 	document.querySelector("#vibrar2").addEventListener("click", function(){navigator.vibrate([1000,750,1000,750]);},false);
-	//document.querySelector("#vibrar_rep").addEventListener("click", setInterval(function(){navigator.vibrate([1000,750,1000,750]);},3500),false);
 	document.querySelector("#vibrar_rep").addEventListener("click",function(){repetir_vibracion();},false);
 	document.querySelector("#vibrar_cont").addEventListener("click", function(){navigator.vibrate(30000);},false);
-	//document.querySelector("#vibrar_apagar").addEventListener("click", function(){navigator.vibrate(0);},false);
 	document.querySelector("#vibrar_apagar").addEventListener("click", function(){parar_vibracion();},false);
 
 }
